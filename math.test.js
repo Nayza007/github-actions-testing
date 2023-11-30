186fc9d9a8a2bcc478fefb25f4d805fc58e9e915
Athisan (Nay) Sereethaweepat <athisan@aisoftthailand.com>
2023-11-30 11:17:11 +0700

    :tada:

---
+++ math.test.js
@@ -0,0 +1,11 @@
+const { add, subtract } = require("./math");
+
+// Test for the add function
+test("adds 1 + 2 to equal 3", () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+// Test for the subtract function
+test("subtracts 4 - 2 to equal 2", () => {
+  expect(subtract(4, 2)).toBe(2);
+});
